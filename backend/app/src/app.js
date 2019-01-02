faa979f9f35fccf39e4329344f1469b9b113c0f7
Bao To <bao2703@gmail.com>
2019-01-02 21:22:27 +0700

    Init

--- backend/app/src/app.js
+++ backend/app/src/app.js
@@ -9,10 +9,10 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
